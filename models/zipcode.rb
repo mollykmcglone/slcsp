306810b80a5a253f17acf9a415507efaca7a3c61
Molly <mollykmcglone@gmail.com>
2019-05-18 19:01:04 -0700

    refactor zipcode methods for better clarity and to handle edge cases

--- models/zipcode.rb
+++ models/zipcode.rb
@@ -1,5 +1,3 @@
-require 'securerandom'
-
 class Zipcode
   FIELDS = [:zipcode, :rate_areas, :plans, :slcsp]
   attr_accessor *FIELDS
@@ -8,33 +6,48 @@ class Zipcode
     self.zipcode = csv_row['zipcode']
   end
 
-  def calculate_slcsp(zip_records, plans)
-    find_associated_rate_areas(zip_records)
+  def determine_second_lowest_cost_silver_plan(county_records = nil, plans = nil)
+    find_associated_rate_areas(county_records)
     find_associated_plans(plans)
-    find_second_lowest_cost_silver_plan
+    if can_determine_slcsp?
+      self.slcsp = get_rate_of_second_lowest_cost_silver_plan
+    else
+      self.slcsp = nil
+    end
   end
 
-  def find_associated_rate_areas(all_zip_records)
-    zip_records_for_zipcode = all_zip_records.select { |zip_record| zip_record.zipcode == self.zipcode }
+  private
 
+  def find_associated_rate_areas(all_county_records = nil)
+    county_records_for_zipcode = all_county_records.select { |record| record.zipcode == self.zipcode }
     rate_areas = []
-    zip_records_for_zipcode.each do |zip_record|
-      rate_area = [zip_record.state, zip_record.rate_area]
-      rate_areas << rate_area
+    county_records_for_zipcode.each do |record|
+      rate_areas << record.rate_area
     end
     self.rate_areas = rate_areas.uniq
   end
 
-  def find_associated_plans(all_plans)
+  def find_associated_plans(all_plans = nil)
     self.plans = all_plans.select { |plan| self.rate_areas.include?(plan.rate_area) }
   end
 
-  def find_second_lowest_cost_silver_plan
-    all_silver_plans = plans.select { |plan| plan.metal_level == 'Silver' }
-    if all_silver_plans.length > 1
-      sorted_plans = all_silver_plans.sort_by { |plan| plan.rate }
-      second_lowest_cost_silver_plan = sorted_plans[1].rate
+  def can_determine_slcsp?
+    # can only determine slcsp if zipcode is present in exactly one rate area
+    self.rate_areas.length == 1
+  end
+
+  def find_silver_plans
+    plans.select { |plan| plan.metal_level == 'Silver' }
+  end
+
+  def get_rate_of_second_lowest_cost_silver_plan
+    silver_plans = find_silver_plans
+    rate = nil
+    # cannot set slcsp if there are no silver plans or only one silver plan
+    if silver_plans && silver_plans.length > 1
+      sorted_plans = silver_plans.sort_by { |plan| plan.rate }
+      rate = sorted_plans[1].rate
     end
-    self.slcsp = second_lowest_cost_silver_plan ? second_lowest_cost_silver_plan : nil
+    rate
   end
 end
