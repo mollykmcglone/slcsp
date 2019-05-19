ce4f1a19f427fa5e56f86cc04e5f1046b81f007f
Molly <mollykmcglone@gmail.com>
2019-05-18 19:01:59 -0700

    add rest of necessary methods to Processor

--- models/processor.rb
+++ models/processor.rb
@@ -1,17 +1,46 @@
 class Processor
-  def self.start
-    zip_records = Importer.import_zip_records
-    puts "Created #{zip_records.length} zip_records!"
+  def start
+    data = initialize_data
+    results = process_plans(data)
+    output(results)
+  end
 
-    plans = Importer.import_plans
-    puts "Created #{plans.length} plans!"
+  private
 
-    zipcodes = Importer.import_zipcodes
-    puts "Created #{zipcodes.length} zipcodes!"
+  def initialize_data
+    data["county_records"] = import_data("zips", "CountyRecord")
+    data["plans"] = import_data("plans", "Plan")
+    data["zipcodes"] = import_data("slcsp", "Zipcode")
+  end
 
-    zipcodes.each do |zipcode|
-      zipcode.calculate_slcsp(zip_records, plans)
+  def import_data(file_name, class_name)
+    file_path = "./csv_files/#{file_name}.csv"
+    if File.exists?(file_path)
+      importer = Importer.new(class_name)
+      items = Importer.import(file_path)
+      puts "Created #{items.length} #{class_name}'s!"
+      items
+    else
+      raise ArgumentError.new("The file #{file_name} does not exist")
+    end
+  end
+
+  def process_plans(data)
+    data["zipcodes"].each do |zipcode|
+      zipcode.determine_second_lowest_cost_silver_plan(data["county_records"], data["plans"])
       puts "SLCSP for zipcode: #{zipcode.zipcode} is #{zipcode.slcsp}"
+    end
+    data
+  end
+
+  def output(results)
+    puts "Result!"
+    headers = ["zipcode", "rate"]
+    CSV.open('./csv_files/slcsp_result.csv','w', :write_headers=> true, :headers => headers) do |file|
+      results["zipcodes"].each do |zipcode|
+        row = [zipcode.zipcode, zipcode.slcsp]
+        file << row
+      end
     end
   end
 end
